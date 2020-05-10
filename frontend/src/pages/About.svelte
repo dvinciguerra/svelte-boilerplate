38571e316e46bae89525afb03b15f94263fa6eff
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2020-05-10 18:35:00 -0300

    feat: add routes to projects

---
+++ frontend/src/pages/About.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { link } from 'svelte-routing';
+</script>
+
+<main>
+	<h1>About Page!</h1>
+  <p>
+    <a href="/" use:link>Home Page</a>
+  </p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
---
+++ frontend/src/pages/Home.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { link } from 'svelte-routing';
+</script>
+
+<main>
+	<h1>Home Page!</h1>
+  <p>
+    <a href="/about" use:link>About Page</a>
+  </p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
